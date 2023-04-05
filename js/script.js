5fdd7536edb57dbd5caeabe22c020b5e3e94ee5c
muffakiribnhamid <muffakir63@gmail.com>
2023-04-05 14:25:19 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let addTodoButton = document.getElementById('add-todo');
+let todoInput = document.getElementById('input-field');
+let container = document.getElementById('container');
+let clearAll = document.getElementById('clear-all');
+
+
+addTodoButton.addEventListener('click', function() {
+
+    // Initialize elements
+    var paragraph = document.createElement('p');
+    var paragraph_div = document.createElement('div');
+
+    // Set Values
+    paragraph.innerText = todoInput.value;
+    todoInput.value = '';
+    
+    //Give Classes
+    paragraph.classList.add('paragraph-styling');
+    container.appendChild(paragraph_div);
+    paragraph_div.classList.add('paragraph_div');
+    paragraph_div.appendChild(paragraph);
+
+    // Add Icon
+
+    const icon = document.createElement('img');
+    icon.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Remove_font_awesome.svg/1024px-Remove_font_awesome.svg.png';
+
+    icon.height = 20;
+
+    document.querySelectorAll('.paragraph_div').forEach(function(paraDiv) {
+        paraDiv.appendChild(icon);
+            });
+
+
+
+    // Add Event Listeners
+
+
+    paragraph_div.addEventListener('click', function() {
+        container.removeChild(paragraph_div);
+    }
+    );
+
+    paragraph.addEventListener('click', function() {
+        paragraph.style.textDecoration = "line-through";
+    });
+
+    paragraph.addEventListener('dblclick', function() {
+        container.removeChild(paragraph);
+    }
+    );
+
+    clearAll.addEventListener('click', function() {
+        container.removeChild(paragraph_div);
+    });
+
+});
+
